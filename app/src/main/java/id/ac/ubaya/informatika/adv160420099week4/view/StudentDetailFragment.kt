4687aa740bf9341530c453065cdfdf03bc921721
Arth100402 <102035801+Arth100402@users.noreply.github.com>
2023-04-05 14:30:33 +0700

    week6 tugaskelas

--- app/src/main/java/id/ac/ubaya/informatika/adv160420099week4/view/StudentDetailFragment.kt
+++ app/src/main/java/id/ac/ubaya/informatika/adv160420099week4/view/StudentDetailFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.textfield.TextInputEditText
 import id.ac.ubaya.informatika.adv160420099week4.R
+import id.ac.ubaya.informatika.adv160420099week4.util.loadImage
 import id.ac.ubaya.informatika.adv160420099week4.viewmodel.DetailViewModel
 
 class StudentDetailFragment : Fragment() {
@@ -24,15 +27,18 @@ class StudentDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        detailViewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
-        detailViewModel.fetch()
-
+        var studentPhoto = view.findViewById<ImageView>(R.id.imageView2)
+        var progressBarDetail = view.findViewById<ProgressBar>(R.id.progressBarDetail)
         var studentID = view.findViewById<TextInputEditText>(R.id.txtID)
         var studentName = view.findViewById<TextInputEditText>(R.id.txtName)
         var birthOfDate = view.findViewById<TextInputEditText>(R.id.txtBod)
         var phone = view.findViewById<TextInputEditText>(R.id.txtPhone)
 
+        detailViewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        detailViewModel.fetch(StudentDetailFragmentArgs.fromBundle(requireArguments()).studentID)
+
         detailViewModel.studentLD.observe(viewLifecycleOwner){studentDetail ->
+            studentPhoto.loadImage(studentDetail.photoUrl, progressBarDetail)
             studentID.setText(studentDetail.id.toString())
             studentName.setText(studentDetail.name.toString())
             birthOfDate.setText(studentDetail.bod.toString())
